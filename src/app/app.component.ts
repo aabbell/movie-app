97cb96366dcdb8162cd1161b5d1daedda84d6e2a
aabbell <abelalemneh68gmail.com>
2025-03-30 11:34:57 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component} from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+          CommonModule,
+          HttpClientModule,
+          MatIconModule,
+          FormsModule,
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'movie-app';
+  searchTerm:string = ''
+  movies:any[] = []
+
+  private API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=a69c7b0e"
+
+  constructor(private http: HttpClient){
+    this.searchMoives('SpiderMan')
+  }
+
+  searchMoives(title:string):void{
+      this.http.get<any>(`${this.API_URL}&s=${title}`).subscribe((response) => { 
+        this.movies = response.Search || []
+      })
+  }
+  onSearch():void{
+    if (this.searchTerm.trim()){
+      this.searchMoives(this.searchTerm)
+    }
+  }
 }
