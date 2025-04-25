09dc4f706c507181849e13b6b8fbc203f974113c
aabbell <abelalemneh68@gmail.com>
2025-04-25 13:52:11 +0300

    none

---
+++ src/app/none/none.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-none',
+  imports: [],
+  templateUrl: './none.component.html',
+  styleUrl: './none.component.css'
+})
+export class NoneComponent {
+
+}
